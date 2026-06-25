9c573c979a1fa455aed7466d353552f511f01190
Omckar <badekaarks@gmail.com>
2026-06-25 13:41:11 +0530

    prod env creds

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://url-manager-backend-production.up.railway.app/api',
+  baseUrl: 'https://url-manager-backend-production.up.railway.app',
+};
